c1f33372ae4e28ed9c320ecdc86ae43174fb7909
Santy <110985959+Santy-Mendez@users.noreply.github.com>
2024-03-15 11:49:37 -0300

    TP_02

---
+++ Ejercicios_POO_python/Ejercicio_3.py
@@ -0,0 +1,24 @@
+class Triangulo:
+    def __init__(self, lado1, lado2, lado3):
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+    
+    def imprimir_lado_mayor(self):
+        lado_mayor = max(self.lado1, self.lado2, self.lado3)
+        print("El lado mayor es:", lado_mayor)
+    
+    def es_equilatero(self):
+        if self.lado1 == self.lado2 == self.lado3:
+            print("El triángulo es equilátero.")
+        else:
+            print("El triángulo no es equilátero.")
+
+
+lado1 = float(input("Ingrese la longitud del primer lado: "))
+lado2 = float(input("Ingrese la longitud del segundo lado: "))
+lado3 = float(input("Ingrese la longitud del tercer lado: "))
+
+triangulo = Triangulo(lado1, lado2, lado3)
+triangulo.imprimir_lado_mayor()
+triangulo.es_equilatero()
