be51a664f9e99deaac136fb4d387817b96cd5106
Santy <110985959+Santy-Mendez@users.noreply.github.com>
2024-03-22 08:15:04 -0300

    Ejercicio_6_Guia2

---
+++ Ejercicios_POO_python/Ejercicio_6.py
@@ -0,0 +1,16 @@
+class Familia:
+    def __init__(self, nombre_padre, nombre_madre, hijos):
+        self.nombre_padre = nombre_padre
+        self.nombre_madre = nombre_madre
+        self.hijos = hijos
+    
+    def __str__(self):
+        hijos_str = ", ".join(self.hijos)
+        return f"Padre: {self.nombre_padre}, Madre: {self.nombre_madre}, Hijos: {hijos_str}"
+
+# Ejemplo de uso:
+if __name__ == "__main__":
+    nombres_hijos = ["Juan", "María", "Pedro"]
+    familia1 = Familia("Carlos", "Ana", nombres_hijos)
+    print(familia1)
+
